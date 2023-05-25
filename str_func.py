a8306c6e010d70a75b0a834f07503d09d788ccf4
Johnny <evgenykiselev@aol.com>
2023-05-25 21:06:53 +0500

    Добавление функции заглавных первых букв

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_func(stri):
     return stri.upper()
+
+
+def word_func(enter):
+    """Функция, которая возвращает каждое слово из строки с первой большой буквой"""
+    return enter.title()
